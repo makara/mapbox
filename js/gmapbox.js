09d12948cf08511efbcad905e3bd5662e12f4c89
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2010-11-01 19:45:40 +0000

    #957202 by sreynen: Better layer names for gmaps maps

--- js/gmapbox.js
+++ js/gmapbox.js
@@ -6,10 +6,10 @@ Drupal.gmap.addHandler('gmap',function(elem) {
     var maps = Drupal.settings.gmapbox.mapdefs;
     // Use .each to avoid scope issues in CustomTilesUrl
     $.each(layers, function(k, val) {    
-      var name = k.replace(/-/gi,' ');
       if (k in maps) {
         var minZoom = maps[k].minzoom;
         var maxZoom = maps[k].maxzoom;
+        var name = maps[k].title;
     	opts.mapTypes.push(GMapBox(k, name, {minZoom: minZoom, maxZoom: maxZoom}));
     	opts.mapTypeNames.push(k); 
       }
